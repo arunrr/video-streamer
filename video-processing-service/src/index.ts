59275290fdff4ccad697c9f10a5ffe575dd09ea1
Arun <arun90rr@gmail.com>
2023-10-07 03:14:22 +0530

    Added logic to convert video resolution

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,4 +1,5 @@
-const express = require('express');
+import express, { Request, Response } from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const PORT = process.env.PORT || 3000;
 
@@ -6,10 +7,32 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(PORT, (err: Error) => {
-  if (err) {
-    console.error(err.message);
-  } else {
-    console.log(`Server started and listening at http://localhost:${PORT}`);
+app.post('/vid-process', (req: Request, res: Response) => {
+  const { inputFilePath, outputFilePath } = req.body;
+
+  if (!inputFilePath) {
+    res
+      .status(400)
+      .json({ error: `input file path is: ${inputFilePath} which is invalid` });
   }
+  if (!outputFilePath) {
+    res.status(400).json({
+      error: `output file path is: ${outputFilePath} which is invalid`,
+    });
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions('-vf', 'scale=-1:360') // 360p
+    .on('end', () => {
+      res.status(200).json({ msg: 'file is processed successfully' });
+    })
+    .on('error', (err) => {
+      console.error(`An error occurred: err.message`);
+      res.status(500).json({ error: 'internal server error' });
+    })
+    .save(outputFilePath);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started and listening at http://localhost:${PORT}`);
 });
